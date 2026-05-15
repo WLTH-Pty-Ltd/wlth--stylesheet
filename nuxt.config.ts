4ad0e2ae192bed0ded7800bb3e9d48a75c446985
ddaannnnyy <hello@danhebdon.com>
2026-05-15 15:58:28 +1000

    feat: :lipstick: Merged css files into single main source

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: [
-    './app/assets/css/reset.css',
-    './app/assets/css/colors.css',
-    './app/assets/css/utilities.css',
-    './app/assets/css/fonts.css',
-    './app/assets/css/buttons.css'
-  ]
+  css: ['./app/assets/css/main.css']
 })
