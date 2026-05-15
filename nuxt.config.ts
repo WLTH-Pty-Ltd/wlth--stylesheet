147cb86a5025a97ed999e2debf6a5ac7b2c8032f
ddaannnnyy <hello@danhebdon.com>
2026-05-15 16:52:17 +1000

    feat: :mag: Added head attrs to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css']
+  css: ['./app/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'WLTH External Styleguide',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        }
+      ]
+    }
+  }
 })
